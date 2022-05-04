8eaa37610508eced942e9f37fe7ab84237b16fb0
FernandoPereira218 <fernandofmp218@gmail.com>
2022-05-04 15:30:50 -0300

    Criação do projeto + configuração do firebase

---
+++ Banco de Dados/Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Cloud.Firestore;
+using sistema_academia.Classes;
+
+namespace sistema_academia.Banco_de_Dados
+{
+    public static class Database
+    {
+
+        public static async Task CriarAlunoAsync()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "sistema-academia.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+            FirestoreDb db = FirestoreDb.Create("sistema-academia-aee3a");
+
+            DocumentReference docRef = db.Collection("teste").Document("alovelace");
+            Dictionary<string, object> user = new Dictionary<string, object>
+            {
+                { "First", "Ada" },
+                { "Last", "Lovelace" },
+                { "Born", 1815 }
+            };
+            _ = await docRef.SetAsync(user);
+
+            MessageBox.Show("a");
+        }
+    }
+}
